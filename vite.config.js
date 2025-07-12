8336a13987eda303413bcb8b1aa5da3816a6dcab
Avivcogo <avivcogo@gmail.com>
2025-07-12 16:42:22 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
+    build: {
+        outDir: "dist",
+    },
+
     plugins: [
         laravel({
             input: [
-                'resources/css/Global.scss',
-                'resources/js/app.js',
-                'resources/js/App.jsx',
-                'resources/js/main.jsx',
+                "resources/css/Global.scss",
+                "resources/js/app.js",
+                "resources/js/App.jsx",
+                "resources/js/main.jsx",
             ],
             refresh: true,
         }),
